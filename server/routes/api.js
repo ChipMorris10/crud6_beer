b747272356a78fbbf114d31fa1d1a67a59ca58ab
ChipMorris10 <goldcomac@mac.com>
2015-09-17 18:29:41 -0600

    routes are set up but I'm having trouble with PUT. I'll look at it later

---
+++ server/routes/api.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var router = express.Router();
+var Beer = require('../models/beer.js');
+
+// ** GET ALL beers ** //
+router.get('/beers', function (req, res, next) {
+    Beer.findQ()
+        .then(function(result){res.json(result)})
+        .fail(function(err){res.send(err)})
+        .done();
+});
+
+// ** GET SINGLE beer ** //
+router.get('/beer/:id', function (req, res, next) {
+    Beer.findByIdQ(req.params.id)
+    .then(function(result){res.json(result)})
+    .fail(function(err){res.send(err)})
+    .done();
+});
+
+
+// ** POST ALL beers ** //
+router.post('/beers', function (req, res, next) {
+
+    new Beer ( {
+        name: req.body.name,
+        hoppy: req.body.hoppy
+})
+    .saveQ()
+        .then(function(result){res.json(result)})
+        .fail(function(err){res.send(err)})
+        .done();
+});
+
+// ** PUT (Update) SINGLE beer ** //
+router.put('/beer/:id', function (req, res, next) {
+    Vegetarian.findByIdAndUpdateQ(req.params.id, req.body)
+        .then(function(result){res.json(result)})
+        .fail(function(err){res.send(err)})
+        .done();
+});
+
+// ** DELETE SINGLE beer ** //
+router.delete('/beer/:id', function (req, res, next) {
+    Beer.findByIdAndRemoveQ(req.params.id)
+        .then(function(result){res.json(result)})
+        .fail(function(err){res.send(err)})
+        .done();
+})
+
+
+module.exports = router;
